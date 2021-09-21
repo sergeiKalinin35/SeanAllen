5cdf680e091bd15ce1b1b513a26b63ff1b1ae593
Sergey <piolersa85@mail.ru>
2021-09-21 22:26:31 +0200

    Initial Commit

---
+++ SeanAllen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeanAllen
+//
+//  Created by Sergey on 21.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
